93638a0cef42621d2235f9c104a260922f302c47
TwigHK <git@twig.hk>
2018-06-23 09:54:20 +0100

    Wait 16ms after sending the keys, to prevent keyboard from being in non-idle state

--- KeyboardFirmware/Src/KeyboardFirmwareMain.cpp
+++ KeyboardFirmware/Src/KeyboardFirmwareMain.cpp
@@ -11,6 +11,7 @@
 #include "usbd_hid.h"
 
 #include "LEDs.h"
+#include "Keycodes.h"
 
 void Blinky();
 void ManualDisco();
@@ -203,89 +204,6 @@ void s(const KeyBits &kb, bool pressed, HidKeyboard& hk){
 	}
 }
 
-const uint8_t K_a = 0x04;
-const uint8_t K_b = 0x05;
-const uint8_t K_c = 0x06;
-const uint8_t K_d = 0x07;
-const uint8_t K_e = 0x08;
-const uint8_t K_f = 0x09;
-const uint8_t K_g = 0x0a;
-const uint8_t K_h = 0x0b;
-const uint8_t K_i = 0x0c;
-const uint8_t K_j = 0x0d;
-const uint8_t K_k = 0x0e;
-const uint8_t K_l = 0x0f;
-const uint8_t K_m = 0x10;
-const uint8_t K_n = 0x11;
-const uint8_t K_o = 0x12;
-const uint8_t K_p = 0x13;
-const uint8_t K_q = 0x14;
-const uint8_t K_r = 0x15;
-const uint8_t K_s = 0x16;
-const uint8_t K_t = 0x17;
-const uint8_t K_u = 0x18;
-const uint8_t K_v = 0x19;
-const uint8_t K_w = 0x1a;
-const uint8_t K_x = 0x1b;
-const uint8_t K_y = 0x1c;
-const uint8_t K_z = 0x1d;
-
-const uint8_t K_1 = 0x1e;
-const uint8_t K_2 = 0x1f;
-const uint8_t K_3 = 0x20;
-const uint8_t K_4 = 0x21;
-const uint8_t K_5 = 0x22;
-const uint8_t K_6 = 0x23;
-const uint8_t K_7 = 0x24;
-const uint8_t K_8 = 0x25;
-const uint8_t K_9 = 0x26;
-const uint8_t K_0 = 0x27;
-
-const uint8_t K_ret = 0x28;
-const uint8_t K_esc = 0x29;
-const uint8_t K_bks = 0x2a;
-const uint8_t K_tab = 0x2b;
-const uint8_t K_space = 0x2c;
-
-const uint8_t K_minus = 0x2d;
-const uint8_t K_equal = 0x2e;
-
-const uint8_t K_sqopen = 0x2f;
-const uint8_t K_sqclose = 0x30;
-const uint8_t K_bslash = 0x31;
-
-const uint8_t K_scolon = 0x33;
-const uint8_t K_quote = 0x34;
-
-const uint8_t K_grave = 0x34;
-
-const uint8_t K_comma = 0x36;
-const uint8_t K_fstop = 0x37;
-const uint8_t K_fslash = 0x38;
-
-const uint8_t K_clock = 0x39;
-
-const uint8_t K_f1 = 0x3a;
-const uint8_t K_f2 = 0x3b;
-const uint8_t K_f3 = 0x3c;
-const uint8_t K_f4 = 0x3d;
-const uint8_t K_f5 = 0x3e;
-const uint8_t K_f6 = 0x3f;
-const uint8_t K_f7 = 0x40;
-const uint8_t K_f8 = 0x41;
-const uint8_t K_f9 = 0x42;
-const uint8_t K_f10 = 0x43;
-const uint8_t K_f11 = 0x44;
-const uint8_t K_f12 = 0x45;
-
-const uint8_t K_del = 0x4c;
-
-const uint8_t K_right = 0x4f;
-const uint8_t K_left = 0x50;
-const uint8_t K_down = 0x51;
-const uint8_t K_up = 0x52;
-
-
 void (*KeyCodes[5][14])(const KeyBits &, bool pressed, HidKeyboard & hk) = 
 	{{k<K_grave>,k<K_1>,k<K_2>,k<K_3>,k<K_4>,k<K_5>
 		 ,s<K_6,K_f6>,s<K_7,K_f7>,s<K_8,K_f8>,s<K_9,K_f9>,s<K_0,K_f10>,s<K_minus,K_f11>,s<K_equal,K_f12>,s<K_bks,K_del>}		
@@ -322,18 +240,20 @@ extern "C" {
 
 struct Hardware hardware;
 
+uint16_t led_brightnesses[] = 
+	{ 0xFFFF
+	, 0x03E0
+	, 0x03C0
+	, 0x0380
+	, 0x0300
+	, 0x0100
+	, 0x0000
+	};
 
 void KFMain(){
 
 	MX_USB_DEVICE_Init();
 	InitLeds();
-
-	//ManualDisco();
-
-	HAL_GPIO_WritePin(BM_EN_GPIO_Port,BM_EN_Pin,GPIO_PIN_RESET); // 
-	HAL_GPIO_WritePin(BMRES_GPIO_Port,BMRES_Pin,GPIO_PIN_RESET); // 
-	HAL_Delay(66);
-	HAL_GPIO_WritePin(BMRES_GPIO_Port,BMRES_Pin,GPIO_PIN_SET); // 
 
 	struct HidKeyboard hidkeyboard_last;
 	while(true){
@@ -341,10 +261,28 @@ void KFMain(){
 		HidKeyboard hidkeyboard = ProcessKeys(hidkeyboard_last);
 
 		if (memcmp(&hidkeyboard_last, &hidkeyboard, sizeof(hidkeyboard)) != 0){
+
 			UsbSend(&hidkeyboard);
+
+			USBD_HID_HandleTypeDef *hhid = (USBD_HID_HandleTypeDef*)hUsbDeviceFS.pClassData;
+			HAL_Delay(16);
+			if(hhid->state != HID_IDLE){
+				SetRLed(0x0000);
+			} else {
+				SetRLed(0xFFFF);
+			}
 		}
 
 		hidkeyboard_last = hidkeyboard;
+		
+		int num_keys_set = 0;
+		for(int i = 0; i != sizeof(hidkeyboard.keys)/sizeof(*hidkeyboard.keys); ++i){
+			if(hidkeyboard.keys[i] != 0){
+				num_keys_set += 1;
+			}
+		}
+			
+		SetGLed(led_brightnesses[num_keys_set]);
 	}
 
 }
