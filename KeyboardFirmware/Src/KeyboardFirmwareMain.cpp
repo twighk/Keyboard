01f30f5782f016dd68ec7d66267d098ac0444fac
TwigHK <git@twig.hk>
2018-06-26 20:02:09 +0100

    blue led when special pressed

--- KeyboardFirmware/Src/KeyboardFirmwareMain.cpp
+++ KeyboardFirmware/Src/KeyboardFirmwareMain.cpp
@@ -204,6 +204,24 @@ void s(const KeyBits &kb, bool pressed, HidKeyboard& hk){
 	}
 }
 
+uint16_t led_brightnesses[] = 
+	{ 0xFFFF
+	, 0x03E0
+	, 0x03C0
+	, 0x0380
+	, 0x0300
+	, 0x0100
+	, 0x0000
+	};
+
+void bled(const KeyBits&, bool pressed, HidKeyboard&){
+	if (pressed){
+		SetBLed(led_brightnesses[2]);
+	} else {
+		SetBLed(0xFFFF);
+	}
+}
+
 void (*KeyCodes[5][14])(const KeyBits &, bool pressed, HidKeyboard & hk) = 
 	{{s<K_grave,K_esc>,s<K_1,K_f1>,s<K_2,K_f2>,s<K_3,K_f3>,s<K_4,K_f4>,s<K_5,K_f5>
 		 ,s<K_6,K_f6>,s<K_7,K_f7>,s<K_8,K_f8>,s<K_9,K_f9>,s<K_0,K_f10>,s<K_minus,K_f11>,s<K_equal,K_f12>,s<K_bks,K_del>}		
@@ -213,8 +231,8 @@ void (*KeyCodes[5][14])(const KeyBits &, bool pressed, HidKeyboard & hk) =
 		 ,k<K_h>,k<K_j>,k<K_k>,s<K_l,K_left>,s<K_scolon,K_down>,s<K_quote,K_right>,nil,k<K_ret>}		
 	,{lshift,s<K_z,K_mute>,s<K_x,K_vol_down>,s<K_c,K_vol_up>,k<K_v>,k<K_b>
 		 ,k<K_n>,k<K_m>,k<K_comma>,k<K_fstop>,k<K_fslash>,nil,nil,rshift}		
-	,{lctrl,k<0>,lgui,lalt,s<K_space,K_del>,nil
-		 ,s<K_space,K_del>,nil,nil,ralt,rgui,k<K_context>,k<0>,rctrl}
+	,{lctrl,bled,lgui,lalt,s<K_space,K_del>,nil
+		 ,s<K_space,K_del>,nil,nil,ralt,rgui,k<K_context>,bled,rctrl}
 	};
 
 static KeyBits previous_kbs = KeyBits();
@@ -240,15 +258,6 @@ extern "C" {
 
 struct Hardware hardware;
 
-uint16_t led_brightnesses[] = 
-	{ 0xFFFF
-	, 0x03E0
-	, 0x03C0
-	, 0x0380
-	, 0x0300
-	, 0x0100
-	, 0x0000
-	};
 
 void KFMain(){
 
